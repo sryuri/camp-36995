b6d839bdcbaf961ccbbca82558207449d0c3d50b
sryuri <yuri.tayama@gmail.com>
2021-11-04 21:37:54 +0900

    Userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :nickname, presence: true
   validates :profile, presence: true
 
 end
