7cecd863d2f02f6c2c90f1dd025722bddefb47de
sryuri <yuri.tayama@gmail.com>
2021-11-04 21:09:15 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :profile, presence: true
+
 end
